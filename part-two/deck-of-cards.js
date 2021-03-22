98f5658deb86477f309f75dcd0e4aa541fe017c1
Kathrin Neyzberg <kneyzberg@gmail.com>
2021-03-22 13:51:31 -0700

    Part 2 Deck of Cards

--- part-two/deck-of-cards.js
+++ part-two/deck-of-cards.js
@@ -2,15 +2,31 @@ $("#get-card").on("click", getCard)
 
 let deckId;
 
-async function getCard(){
-  let result = await axios.get(`https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=1`)
-  console.log(result);
-  console.log(`${result.data.cards[0].value} of ${result.data.cards[0].suit}`);
-}
-
 $(getNewDeck)
 
+async function getCard(){
+  try {
+    let result = await axios.get(`https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=1`)
+    // console.log(result);
+    // console.log(`${result.data.cards[0].value} of ${result.data.cards[0].suit}`);
+    let cardRotation = getRandomRotation();
+    $("#card-results").append($(`<img src="${result.data.cards[0].image}">`).css("transform", `rotate(${cardRotation}deg)`));
+  } catch(e){
+    $("#deck-err").text("There are no more cards in the deck!");
+  }
+}
+
+
+
 async function getNewDeck(){
-  let newDeck = await axios.get("https://deckofcardsapi.com/api/deck/new/");
+  let newDeck = await axios.get("https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1");
   deckId = newDeck.data.deck_id;
+}
+
+function getRandomRotation(){
+  let rotation = Math.floor(Math.random() * (45));
+  if(Math.random() > 0.5){
+    rotation = rotation * -1;
+  }
+  return rotation;
 }
