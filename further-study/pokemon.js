f9f9a0bc4959d967a630706631c16f3753a720ab
m-zz <marcusyzhu@gmail.com>
2021-03-22 15:09:59 -0700

    further study functionality complete

---
+++ further-study/pokemon.js
@@ -0,0 +1,53 @@
+async function getPokemon () {
+  let id = Math.floor(Math.random() * numPoke);
+  let mainResult;
+  
+  try {
+    mainResult = await axios.get(`https://pokeapi.co/api/v2/pokemon/${id}/`);
+  } catch (e) {
+    while (!mainResult) {
+      mainResult = await axios.get(`https://pokeapi.co/api/v2/pokemon/${id}/`);
+    }
+  }
+  
+  let image = mainResult.data.sprites.front_default;
+  let name = mainResult.data.name;
+  let speciesResult;
+
+  try {
+    speciesResult = await axios.get(`https://pokeapi.co/api/v2/pokemon-species/${name}/`);
+  } catch (e) {
+    speciesResult = await axios.get(`https://pokeapi.co/api/v2/pokemon-species/${mainResult.data.species.name}/`);
+  }
+  
+  let description;
+
+  for (let result of speciesResult.data.flavor_text_entries) {
+    if (result.language.name === "en") {
+      description = result.flavor_text;
+      break;
+    }
+  }
+  
+  pokemon[name] = {image, name, description};
+
+  displayPokemon(name);
+}
+
+function displayPokemon(name) {
+  $("#poke-gallery").append($(`<div class="poke-card">
+                                <p><b>${pokemon[name].name}</b></p>
+                                <img src="${pokemon[name].image}">
+                                <p>${pokemon[name].description}</p>
+                              </div>`));
+}
+
+let numPoke;
+let pokemon = {};
+
+$(async function start() {
+  numPoke = await axios.get("https://pokeapi.co/api/v2/pokemon?limit=2000");
+  numPoke = numPoke.data.results.length;
+});
+
+$("#get-poke").on("click", getPokemon);
